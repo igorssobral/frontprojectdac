b8a294579f925e04e78c3a87b7bf049205a39038
Igor Sobral <igorgmz1421@gmail.com>
2023-04-03 00:56:08 -0300

    committed

--- src/TelaCurso.js
+++ src/TelaCurso.js
@@ -12,23 +12,23 @@ function TelaCurso(props) {
       <form>
         <label>
           Nome
-          <input type="text" name="nome" placeholder='Nome da instituição' />
+          <input type="text" name="nome" maxLength={40} placeholder='Nome do curso' />
         </label>
         <label>
           Carga Horária
-          <input type="number" name="cargahoraria" placeholder='Digite a Carga horária' />
+          <input type="number" name="cargahoraria" min = {1} max={15000} placeholder='Digite a Carga horária' />
         </label>
         <label>
           Períodos
-          <input type='number' name="periodos" placeholder='Quantidade de periodos' />
+          <input type='number' name="periodos" min = {1} max={15} placeholder='Quantidade de periodos' />
         </label>
         <label>
           Mensalidade
-          <input type='number' name="mensalidade" placeholder='Quantidade de periodos' />
+          <input type='number' name="mensalidade"  min = {1} max={20000}  placeholder='Quantidade de periodos' />
         </label>
         <label>
           InstituicaoID
-          <input type='number' name="instituicaoID" placeholder='ID da insituicao que pertence' />
+          <input type='number' name="instituicaoID" min = {1}  placeholder='ID da insituicao que pertence' />
         </label>
         <br />
         <button type="submit">Salvar</button>
